6dc4ea53066978f2b86a67881fbcf185f095191b
Muhammad Ihsan <ajihsan96@gmail.com>
2018-07-03 13:21:24 +0700

    logot

--- logout.php
+++ logout.php
@@ -2,6 +2,6 @@
 require_once('gauth.php');
 if(session_destroy()) // Destroying All Sessions
 {
-header("Location: page_landing.php"); // Redirecting To Home Page
+header("Location: https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://localhost/evote/page_landing.php"); // Redirecting To Home Page
 }
 ?>
